2d3128f6772ebb437eab06ed627d6d0a1fbb8ee2
lucien-george <lucien.george@mail.mcgill.ca>
2017-10-14 15:45:34 -0400

    Test Added: setValue()

--- src/test/java/ecse429/group10/MapIteratorTest.java
+++ src/test/java/ecse429/group10/MapIteratorTest.java
@@ -40,309 +40,360 @@ import static org.junit.Assert.fail;
 @RunWith(JukitoRunner.class)
 public class MapIteratorTest {
 
-    Cloner cloner = new Cloner();
+	Cloner cloner = new Cloner();
 
-    public static class Module extends JukitoModule {
+	public static class Module extends JukitoModule {
 
-         @Override
+        @Override
         protected void configureTest() {
 
-             // generic binds
-             bindMany(IterableMap.class , ListOrderedMap.class , LinkedMap.class, BidiMap.class);
+            // generic binds
+            bindMany(IterableMap.class, ListOrderedMap.class, LinkedMap.class, BidiMap.class);
 
-             // initialize empty instances of maps
-             bindManyNamedInstances(IterableMap.class, "empty" , new ListOrderedMap() , new LinkedMap(), new TreeBidiMap());
+            // initialize empty instances of maps
+            bindManyNamedInstances(IterableMap.class, "empty", new ListOrderedMap(), new LinkedMap(), new TreeBidiMap());
 
-             // initialize non-empty instances of maps
-             bindManyNamedInstances(IterableMap.class, "filled" , new ListOrderedMap<String , Integer>()
-                     {{
-                        put("ONE" , 1);
-                        put("TWO" , 2);
-                     }},
-                     new LinkedMap<String , Integer>()
-                     {{
-                         put("ONE" , 1);
-                         put("TWO" , 2);
-                     }},
-                     new TreeBidiMap<String , Integer>()
-                     {{
-                         put("ONE" , 1);
-                         put("TWO" , 2);
-                     }});
+            // initialize non-empty one element instances of maps
+            bindManyNamedInstances(IterableMap.class, "one element", new ListOrderedMap<String, Integer>() {{
+                        put("ONE", 1);
+                    }},
+                    new LinkedMap<String, Integer>() {{
+                        put("ONE", 1);
+                    }},
+                    new TreeBidiMap<String, Integer>() {{
+                        put("ONE", 1);
+                    }});
 
-             // initialize non-empty one element instances of maps
-             bindManyNamedInstances(IterableMap.class, "one element" , new ListOrderedMap<String , Integer>()
-                     {{
-                         put("ONE" , 1);
-                     }},
-                     new LinkedMap<String , Integer>()
-                     {{
-                         put("ONE" , 1);
-                     }},
-                     new TreeBidiMap<String , Integer>()
-                     {{
-                         put("ONE" , 1);
-                     }});
-         }
-
-
+            // initialize non-empty instances of maps
+            bindManyNamedInstances(IterableMap.class, "filled", new ListOrderedMap<String, Integer>() {{
+                        put("ONE", 1);
+                        put("TWO", 2);
+                    }},
+                    new LinkedMap<String, Integer>() {{
+                        put("ONE", 1);
+                        put("TWO", 2);
+                    }},
+                    new TreeBidiMap<String, Integer>() {{
+                        put("ONE", 1);
+                        put("TWO", 2);
+                    }});
+        }
     }
 
-    /**
-     *
-     * Test that there is no next element in an empty map
-     * @param map
-     *
-     * */
-    @Test(expected = NoSuchElementException.class)
-    public void failAfterLastElementEmptyMap(@All("empty") IterableMap map) {
-        MapIterator iterator = map.mapIterator();
-        iterator.next();
-    }
+	/**
+	 *
+	 * Test that there is no next element in an empty map
+	 * @param map
+	 *
+	 * */
+	@Test(expected = NoSuchElementException.class)
+	public void failAfterLastElementEmptyMap(@All("empty") IterableMap map) {
+		MapIterator iterator = map.mapIterator();
+		iterator.next();
+	}
 
-    /**
-     *
-     * Test that there is no second element in map of size 1 by calling next() twice
-     * @param map
-     *
-     * */
-    @Test(expected = NoSuchElementException.class)
-    public void failAfterLastElementOnedMap(@All("one element") IterableMap map) {
-        MapIterator iterator = map.mapIterator();
-        iterator.next();
-        iterator.next();
-    }
+	/**
+	 *
+	 * Test that there is no second element in map of size 1 by calling next() twice
+	 * @param map
+	 *
+	 * */
+	@Test(expected = NoSuchElementException.class)
+	public void failAfterLastElementOnedMap(@All("one element") IterableMap map) {
+		MapIterator iterator = map.mapIterator();
+		iterator.next();
+		iterator.next();
+	}
 
-    /**
-     *
-     * Test that there is no fourth element in map of size 2 by calling next() 3 times
-     * @param map
-     *
-     * */
-    @Test(expected = NoSuchElementException.class)
-    public void failAfterLastElementFilledMap(@All("filled") IterableMap map) {
-        MapIterator iterator = map.mapIterator();
-        iterator.next();
-        iterator.next();
-        iterator.next();
-    }
+	/**
+	 *
+	 * Test that there is no fourth element in map of size 2 by calling next() 3 times
+	 * @param map
+	 *
+	 * */
+	@Test(expected = NoSuchElementException.class)
+	public void failAfterLastElementFilledMap(@All("filled") IterableMap map) {
+		MapIterator iterator = map.mapIterator();
+		iterator.next();
+		iterator.next();
+		iterator.next();
+	}
 
-    /**
-     *
-     * Test that Test that getKey() returns an exception on an empty list
-     * @param map
-     *
-     * */
-    @Test(expected = NoSuchElementException.class)
-    public void failGetKeyEmptyMap(@All("empty") IterableMap map)
-    {
-        MapIterator iterator = map.mapIterator();
-        iterator.next();
-        assertEquals("ONE" , iterator.getKey());
-    }
+	/**
+	 *
+	 * Test that Test that getKey() returns an exception on an empty list
+	 * @param map
+	 *
+	 * */
+	@Test(expected = NoSuchElementException.class)
+	public void failGetKeyEmptyMap(@All("empty") IterableMap map)
+	{
+		MapIterator iterator = map.mapIterator();
+		iterator.next();
+		assertEquals("ONE" , iterator.getKey());
+	}
 
-    /**
-     *
-     * Test that Test that getKey() returns the correct key in a map of size 1
-     * @param map
-     *
-     * */
-    @Test
-    public void failGetKeyOneMap(@All("one element") IterableMap map)
-    {
-        MapIterator iterator = map.mapIterator();
-        iterator.next();
-        assertEquals("ONE" , iterator.getKey());
-    }
+	/**
+	 *
+	 * Test that Test that getKey() returns the correct key in a map of size 1
+	 * @param map
+	 *
+	 * */
+	@Test
+	public void failGetKeyOneMap(@All("one element") IterableMap map)
+	{
+		MapIterator iterator = map.mapIterator();
+		iterator.next();
+		assertEquals("ONE" , iterator.getKey());
+	}
 
-    /**
-     *
-     * Test that Test that getKey() returns the correct key in a map of size 2
-     * @param map
-     *
-     * */
-    @Test
-    public void failGetKeyFilledMap(@All("filled") IterableMap map)
-    {
-        MapIterator iterator = map.mapIterator();
-        iterator.next();
-        assertEquals("ONE" , iterator.getKey());
-    }
+	/**
+	 *
+	 * Test that Test that getKey() returns the correct key in a map of size 2
+	 * @param map
+	 *
+	 * */
+	@Test
+	public void failGetKeyFilledMap(@All("filled") IterableMap map)
+	{
+		MapIterator iterator = map.mapIterator();
+		iterator.next();
+		assertEquals("ONE" , iterator.getKey());
+	}
 
-    /**
-     *
-     * Test that Test that getValue() returns an exception on an empty list
-     * @param map
-     *
-     * */
-    @Test(expected = NoSuchElementException.class)
-    public void failGetValueEmptyMap(@All("empty") IterableMap map)
-    {
-        MapIterator iterator = map.mapIterator();
-        iterator.next();
-        assertEquals(1 , iterator.getValue());
-    }
+	/**
+	 *
+	 * Test that Test that getValue() returns an exception on an empty list
+	 * @param map
+	 *
+	 * */
+	@Test(expected = NoSuchElementException.class)
+	public void failGetValueEmptyMap(@All("empty") IterableMap map)
+	{
+		MapIterator iterator = map.mapIterator();
+		iterator.next();
+		assertEquals(1 , iterator.getValue());
+	}
 
-    /**
-     *
-     * Test that Test that getValue() returns the correct key in a map of size 1
-     * @param map
-     *
-     * */
-    @Test
-    public void failGetValueOneMap(@All("one element") IterableMap map)
-    {
-        MapIterator iterator = map.mapIterator();
-        iterator.next();
-        assertEquals(1 , iterator.getValue());
-    }
+	/**
+	 *
+	 * Test that Test that getValue() returns the correct key in a map of size 1
+	 * @param map
+	 *
+	 * */
+	@Test
+	public void failGetValueOneMap(@All("one element") IterableMap map)
+	{
+		MapIterator iterator = map.mapIterator();
+		iterator.next();
+		assertEquals(1 , iterator.getValue());
+	}
 
-    /**
-     *
-     * Test that Test that getValue() returns the correct key in a map of size 2
-     * @param map
-     *
-     * */
-    @Test
-    public void failGetValueFilledMap(@All("filled") IterableMap map)
-    {
-        MapIterator iterator = map.mapIterator();
-        iterator.next();
-        assertEquals(1 , iterator.getValue());
-    }
+	/**
+	 *
+	 * Test that Test that getValue() returns the correct key in a map of size 2
+	 * @param map
+	 *
+	 * */
+	@Test
+	public void failGetValueFilledMap(@All("filled") IterableMap map)
+	{
+		MapIterator iterator = map.mapIterator();
+		iterator.next();
+		assertEquals(1 , iterator.getValue());
+	}
 
-    /**
-     *
-     * Test that the next() method returns the next element in map of size 1
-     * @param map
-     *
-     * */
-    @Test
-    public void shouldReturnNextElementOneElementMap(@All("one element") IterableMap map) {
-        MapIterator iterator = map.mapIterator();
-        Object key = iterator.next();
-        assertEquals("ONE" , key);
-        assertEquals(1 , iterator.getValue());
-    }
+	/**
+	 *
+	 * Test that the next() method returns the next element in map of size 1
+	 * @param map
+	 *
+	 * */
+	@Test
+	public void shouldReturnNextElementOneElementMap(@All("one element") IterableMap map) {
+		MapIterator iterator = map.mapIterator();
+		Object key = iterator.next();
+		assertEquals("ONE" , key);
+		assertEquals(1 , iterator.getValue());
+	}
 
-    /**
-     *
-     * Test that the next() method returns the next element in map of size 2
-     * @param map
-     *
-     * */
-    @Test
-    public void shouldReturnNextElementFilledMap(@All("filled") IterableMap map) {
-        MapIterator iterator = map.mapIterator();
-        Object key = iterator.next();
-        assertEquals("ONE" , key);
-        assertEquals(1 , iterator.getValue());
-    }
+	/**
+	 *
+	 * Test that the next() method returns the next element in map of size 2
+	 * @param map
+	 *
+	 * */
+	@Test
+	public void shouldReturnNextElementFilledMap(@All("filled") IterableMap map) {
+		MapIterator iterator = map.mapIterator();
+		Object key = iterator.next();
+		assertEquals("ONE" , key);
+		assertEquals(1 , iterator.getValue());
+	}
 
-    /**
-     *
-     * Test that hasNext() returns false when called on an empty map
-     * @param map
-     *
-     * */
-    @Test
-    public void shouldReturnFalseHasNextEmpty(@All("empty") IterableMap map) {
-        MapIterator iterator = map.mapIterator();
-        assertFalse(iterator.hasNext());
-    }
+	/**
+	 *
+	 * Test that hasNext() returns false when called on an empty map
+	 * @param map
+	 *
+	 * */
+	@Test
+	public void shouldReturnFalseHasNextEmpty(@All("empty") IterableMap map) {
+		MapIterator iterator = map.mapIterator();
+		assertFalse(iterator.hasNext());
+	}
 
-    /**
-     *
-     * Test that hasNext() returns false when called on last element of a map of size 1
-     * @param map
-     *
-     * */
-    @Test
-    public void shouldReturnFalseHasNextOneElement(@All("one element") IterableMap map) {
-        MapIterator iterator = map.mapIterator();
-        iterator.next();
-        assertFalse(iterator.hasNext());
-    }
+	/**
+	 *
+	 * Test that hasNext() returns false when called on last element of a map of size 1
+	 * @param map
+	 *
+	 * */
+	@Test
+	public void shouldReturnFalseHasNextOneElement(@All("one element") IterableMap map) {
+		MapIterator iterator = map.mapIterator();
+		iterator.next();
+		assertFalse(iterator.hasNext());
+	}
 
-    /**
-     *
-     * Test that hasNext() returns false when called on last element of a map of size 2
-     * @param map
-     *
-     * */
-    @Test
-    public void shouldReturnFalseHasNextFilledElement(@All("filled") IterableMap map) {
-        MapIterator iterator = map.mapIterator();
-        iterator.next();
-        iterator.next();
-        assertFalse(iterator.hasNext());
-    }
+	/**
+	 *
+	 * Test that hasNext() returns false when called on last element of a map of size 2
+	 * @param map
+	 *
+	 * */
+	@Test
+	public void shouldReturnFalseHasNextFilledElement(@All("filled") IterableMap map) {
+		MapIterator iterator = map.mapIterator();
+		iterator.next();
+		iterator.next();
+		assertFalse(iterator.hasNext());
+	}
 
-    /**
-     *
-     * Test that hasNext() returns true when called on map of size 1
-     * @param map
-     *
-     * */
-    @Test
-    public void shouldReturnTrueHasNextOneElement(@All("one element") IterableMap map) {
-        MapIterator iterator = map.mapIterator();
-        assertTrue(iterator.hasNext());
-    }
+	/**
+	 *
+	 * Test that hasNext() returns true when called on map of size 1
+	 * @param map
+	 *
+	 * */
+	@Test
+	public void shouldReturnTrueHasNextOneElement(@All("one element") IterableMap map) {
+		MapIterator iterator = map.mapIterator();
+		assertTrue(iterator.hasNext());
+	}
 
-    /**
-     *
-     * Test that hasNext() returns true when called on a map of size 2
-     * @param map
-     *
-     * */
-    @Test
-    public void shouldReturnTrueHasNextFilled(@All("filled") IterableMap map) {
-        MapIterator iterator = map.mapIterator();
-        assertTrue(iterator.hasNext());
-    }
+	/**
+	 *
+	 * Test that hasNext() returns true when called on a map of size 2
+	 * @param map
+	 *
+	 * */
+	@Test
+	public void shouldReturnTrueHasNextFilled(@All("filled") IterableMap map) {
+		MapIterator iterator = map.mapIterator();
+		assertTrue(iterator.hasNext());
+	}
 
-    /**
-     *
-     * Test that calling remove() on an empty map throws an exception
-     * @param map
-     *
-     * */
-    @Test(expected = IllegalStateException.class)
-    public void failRemoveElementFromEmptyMap(@All("empty") IterableMap map) {
-        IterableMap copiedMap = cloner.deepClone(map);
-        MapIterator iterator = copiedMap.mapIterator();
-        iterator.remove();
-    }
+	/**
+	 *
+	 * Test that calling remove() on an empty map throws an exception
+	 * @param map
+	 *
+	 * */
+	@Test(expected = IllegalStateException.class)
+	public void failRemoveElementFromEmptyMap(@All("empty") IterableMap map) {
+		IterableMap copiedMap = cloner.deepClone(map);
+		MapIterator iterator = copiedMap.mapIterator();
+		iterator.remove();
+	}
 
-    /**
-     *
-     * Test that remove() removes an element from a map of size 1
-     * @param map
-     *
-     * */
-    @Test
-    public void shouldRemoveOneElement(@All("one element") IterableMap map) {
-        IterableMap copiedMap = cloner.deepClone(map);
-        MapIterator iterator = copiedMap.mapIterator();
-        iterator.next();
-        iterator.remove();
-        assertFalse(iterator.hasNext());
-    }
+	/**
+	 *
+	 * Test that remove() removes an element from a map of size 1
+	 * @param map
+	 *
+	 * */
+	@Test
+	public void shouldRemoveOneElement(@All("one element") IterableMap map) {
+		IterableMap copiedMap = cloner.deepClone(map);
+		MapIterator iterator = copiedMap.mapIterator();
+		iterator.next();
+		iterator.remove();
+		assertEquals(0 , copiedMap.size());
+	}
 
-    /**
-     *
-     * Test that Test that remove() removes an element from a map of size 2
-     * @param map
-     *
-     * */
-    @Test
-    public void shouldRemoveFilledElement(@All("filled") IterableMap map) {
-        IterableMap copiedMap = cloner.deepClone(map);
-        MapIterator iterator = copiedMap.mapIterator();
-        iterator.next();
-        iterator.remove();
-        iterator.next();
-        assertEquals("TWO" , iterator.getKey());
-    }
+	/**
+	 *
+	 * Test that Test that remove() removes an element from a map of size 2
+	 * @param map
+	 *
+	 * */
+	@Test
+	public void shouldRemoveFilledElement(@All("filled") IterableMap map) {
+		IterableMap copiedMap = cloner.deepClone(map);
+		MapIterator iterator = copiedMap.mapIterator();
+		iterator.next();
+		iterator.remove();
+		assertEquals(1 , copiedMap.size());
+	}
+
+	/**
+	 *
+	 * Test that calling setValue() on an empty map throws an exception
+	 * @param map
+	 *
+	 * */
+	@Test(expected = NoSuchElementException.class)
+	public void failsetValueElementFromEmptyMap(@All("empty without TreeBidiMap") IterableMap map) {
+		IterableMap copiedMap = cloner.deepClone(map);
+		MapIterator iterator = copiedMap.mapIterator();
+		iterator.next();
+		iterator.setValue(4);
+
+	}
+
+	/**
+	 *
+	 * Test that setValue() modifies an element from a map of size 1
+	 * @param map
+	 *
+	 * */
+	@Test
+	public void failSetValueOneElement(@All("one element without TreeBidiMap") IterableMap map) {
+		IterableMap copiedMap = cloner.deepClone(map);
+		MapIterator iterator = copiedMap.mapIterator();
+		iterator.next();
+		iterator.setValue(4);
+		assertEquals(4 , iterator.getValue());
+	}
+
+	/**
+	 *
+	 * Test that Test that setValue() modifies an element from a map of size 2
+	 * @param map
+	 *
+	 * */
+	@Test
+	public void failSetValueFilledElement(@All("filled without TreeBidiMap") IterableMap map) {
+		IterableMap copiedMap = cloner.deepClone(map);
+		MapIterator iterator = copiedMap.mapIterator();
+		iterator.next();
+		iterator.setValue(4);
+		assertEquals(4 , iterator.getValue());
+	}
+
+	/**
+	 *
+	 * Test that Test that setValue() with TreeBidiMaps throws an exception
+	 * @param tbm -> TreeBidiMap
+	 *
+	 * */
+	@Test(expected = UnsupportedOperationException.class)
+	public void failSetValueFilledTreeBidiMap(TreeBidiMap tbm) {
+		tbm.put("ONE" , 1);
+		MapIterator iterator = tbm.mapIterator();
+		iterator.next();
+		iterator.setValue(4);
+		assertEquals(4 , iterator.getValue());
+	}
 }
