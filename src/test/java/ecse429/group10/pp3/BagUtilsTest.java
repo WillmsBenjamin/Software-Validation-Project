98d478d6d89ab3050d72dd9724d988a04f44be17
Benjamin Willms <benjamin.willms@mail.mcgill.ca>
2017-11-27 17:00:54 -0500

    BagUtilsTest Complete
    Added tests for every method (except the private constructor which is
    commented as such: "Instantiation of BagUtils is not intended or
    required."
    Tested null inputs, as the intended behavior on null input is mentioned
    in each method's documentation. This can be considered using equivalence
    classes (Black box testing technique), as all non-null inputs are
    treated the same by these methods, while null is treated different.

---
+++ src/test/java/ecse429/group10/pp3/BagUtilsTest.java
@@ -0,0 +1,390 @@
+package ecse429.group10.pp3;
+
+import org.apache.commons.collections4.BagUtils;
+import org.apache.commons.collections4.Bag;
+import org.apache.commons.collections4.SortedBag;
+import org.apache.commons.collections4.bag.*;
+
+import org.apache.commons.collections4.functors.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ben on 11/27/17.
+ */
+
+public class BagUtilsTest {
+
+    /*
+     * As explained in project part 2, BagUtils depends on Bag and SortedBag.
+     * As SortedBag extends Bag, we can use a SortedBag instance to test all
+     * the methods in BagUtils. TreeBag "is the standard implementation of a sorted bag"
+     * according to the apache commons collections documentation, and so it
+     * was chosen as the single class used to replace the necessary stub/mock
+     * to test BagUtils (we can assume TreeBag is already tested).
+     */
+    private TreeBag bagInstance;
+    private ArrayList<Integer> items;
+
+    /*
+     * To setup the tests, the bag is filled with 4 unique items, to allow checking of contents, and order.
+     */
+    @Before
+    public void setUp() {
+        this.bagInstance = new TreeBag<Integer>();
+        this.items = new ArrayList<>();
+        this.items.add(5);
+        this.items.add(3);
+        this.items.add(16);
+        this.items.add(1);
+        this.bagInstance.addAll(items);
+    }
+
+    @After
+    public void tearDown() {
+        this.bagInstance = null;
+        items = null;
+    }
+
+    /*
+     * Test for method "synchronizedBag(final Bag<E> bag)".
+     * It checks whether the method returns a SynchronizedBag type.
+     */
+    @Test
+    public void shouldReturnSynchronizedBag() {
+        Bag returnedBag = BagUtils.synchronizedBag(bagInstance);
+        assertTrue(returnedBag instanceof SynchronizedBag);
+        assertTrue(returnedBag.containsAll(items));
+        assertEquals(4, returnedBag.size());
+    }
+
+    /*
+     * Test for method "synchronizedBag(final Bag<E> bag)".
+     * It checks whether the method throws a null pointer exception on null input.
+     */
+    @Test
+    public void shouldReturnSynchronizedBagNullInput() {
+        try {
+            BagUtils.synchronizedBag(null);
+        } catch (Exception e) {
+            assertTrue(e instanceof NullPointerException);
+            return;
+        }
+        fail();
+    }
+
+    /*
+     * Test for method "unmodifiableBag(final Bag<E> bag)".
+     * It checks whether the method returns an UnmodifiableBag type.
+     */
+    @Test
+    public void shouldReturnUnmodifiableBag() {
+        Bag returnedBag = BagUtils.unmodifiableBag(bagInstance);
+        assertTrue(returnedBag instanceof UnmodifiableBag);
+        assertTrue(returnedBag.containsAll(items));
+        assertEquals(4, returnedBag.size());
+    }
+
+    /*
+     * Test for method "unmodifiableBag(final Bag<E> bag)".
+     * It checks whether the method throws a null pointer exception on null input.
+     */
+    @Test
+    public void shouldReturnUnmodifiableBagNullInput() {
+        try {
+            BagUtils.unmodifiableBag(null);
+        } catch (Exception e) {
+            assertTrue(e instanceof NullPointerException);
+            return;
+        }
+        fail();
+    }
+
+    /*
+     * Test for method "predicatedBag(final Bag<E> bag, final Predicate<? super E> predicate)".
+     * It checks whether the method returns a PredicatedBag type with the specified predicate (not null).
+     */
+    @Test
+    public void shouldReturnPredicatedBag() {
+        Bag returnedBag = BagUtils.predicatedBag(bagInstance, NotNullPredicate.INSTANCE);
+        assertTrue(returnedBag instanceof PredicatedBag);
+        assertTrue(returnedBag.containsAll(items));
+        assertEquals(4, returnedBag.size());
+        try {
+            returnedBag.add(null);
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+            assertEquals(4, returnedBag.size());
+            return;
+        }
+        fail();
+    }
+
+    /*
+     * Test for method "predicatedBag(final Bag<E> bag, final Predicate<? super E> predicate)".
+     * It checks whether the method throws a null pointer exception on null input (bag, or predicate).
+     */
+    @Test
+    public void shouldReturnPredicatedBagNullInput() {
+        int i = 0;
+        try {
+            BagUtils.predicatedBag(null, FalsePredicate.INSTANCE);
+        } catch (Exception e) {
+            assertTrue(e instanceof NullPointerException);
+            i++;
+        }
+        if(i == 0) {
+            fail();
+        }
+        try {
+            BagUtils.predicatedBag(bagInstance, null);
+        } catch (Exception e) {
+            assertTrue(e instanceof NullPointerException);
+            return;
+        }
+        fail();
+    }
+
+    /*
+     * Test for method "transformingBag(final Bag<E> bag, final Transformer<? super E, ? extends E> transformer)".
+     * It checks whether the method returns a TransformedBag type with the specified transformer.
+     */
+    @Test
+    public void shouldReturnTransformedBag() {
+        ConstantTransformer trans = new ConstantTransformer(77);
+        Bag returnedBag = BagUtils.transformingBag(bagInstance, trans);
+        assertTrue(returnedBag instanceof TransformedBag);
+        assertTrue(returnedBag.containsAll(items));
+        assertEquals(4, returnedBag.size());
+        returnedBag.add(30);
+        assertTrue(returnedBag.contains(77));
+        assertEquals(5, returnedBag.size());
+    }
+
+    /*
+     * Test for method "transformingBag(final Bag<E> bag, final Transformer<? super E, ? extends E> transformer)".
+     * It checks whether the method throws a null pointer exception on null input (bag, or transformer).
+     */
+    @Test
+    public void shouldReturnTransformedBagNullInput() {
+        int i = 0;
+        try {
+            BagUtils.transformingBag(null, new PredicateTransformer(NotNullPredicate.INSTANCE));
+        } catch (Exception e) {
+            assertTrue(e instanceof NullPointerException);
+            i++;
+        }
+        if(i == 0) {
+            fail();
+        }
+        try {
+            BagUtils.transformingBag(bagInstance, null);
+        } catch (Exception e) {
+            assertTrue(e instanceof NullPointerException);
+            return;
+        }
+        fail();
+    }
+
+    /*
+     * Test for method "collectionBag(final Bag<E> bag)".
+     * It checks whether the method returns a CollectionBag type.
+     */
+    @Test
+    public void shouldReturnCollectionBag() {
+        Bag returnedBag = BagUtils.collectionBag(bagInstance);
+        assertTrue(returnedBag instanceof CollectionBag);
+        assertTrue(returnedBag.containsAll(items));
+        assertEquals(4, returnedBag.size());
+    }
+
+    /*
+     * Test for method "collectionBag(final Bag<E> bag)".
+     * It checks whether the method throws a null pointer exception on null input.
+     */
+    @Test
+    public void shouldReturnCollectionBagNullInput() {
+        try {
+            BagUtils.collectionBag(null);
+        } catch (Exception e) {
+            assertTrue(e instanceof NullPointerException);
+            return;
+        }
+        fail();
+    }
+
+    /*
+     * Test for method "synchronizedSortedBag(final SortedBag<E> bag)".
+     * It checks whether the method returns a SynchronizedSortedBag type.
+     */
+    @Test
+    public void shouldReturnSynchronizedSortedBag() {
+        SortedBag returnedBag = BagUtils.synchronizedSortedBag(bagInstance);
+        assertTrue(returnedBag instanceof SynchronizedSortedBag);
+        assertTrue(returnedBag.containsAll(items));
+        assertEquals(4, returnedBag.size());
+        assertEquals(1, returnedBag.first());
+        assertEquals(16, returnedBag.last());
+    }
+
+    /*
+     * Test for method "synchronizedSortedBag(final SortedBag<E> bag)".
+     * It checks whether the method throws a null pointer exception on null input.
+     */
+    @Test
+    public void shouldReturnSynchronizedSortedBagNullInput() {
+        try {
+            BagUtils.synchronizedSortedBag(null);
+        } catch (Exception e) {
+            assertTrue(e instanceof NullPointerException);
+            return;
+        }
+        fail();
+    }
+
+    /*
+     * Test for method "unmodifiableSortedBag(final SortedBag<E> bag)".
+     * It checks whether the method returns an UnmodifiableBag type.
+     */
+    @Test
+    public void shouldReturnUnmodifiableSortedBag() {
+        SortedBag returnedBag = BagUtils.unmodifiableSortedBag(bagInstance);
+        assertTrue(returnedBag instanceof UnmodifiableSortedBag);
+        assertTrue(returnedBag.containsAll(items));
+        assertEquals(4, returnedBag.size());
+        assertEquals(1, returnedBag.first());
+        assertEquals(16, returnedBag.last());
+    }
+
+    /*
+     * Test for method "unmodifiableSortedBag(final SortedBag<E> bag)".
+     * It checks whether the method throws a null pointer exception on null input.
+     */
+    @Test
+    public void shouldReturnUnmodifiableSortedBagNullInput() {
+        try {
+            BagUtils.unmodifiableSortedBag(null);
+        } catch (Exception e) {
+            assertTrue(e instanceof NullPointerException);
+            return;
+        }
+        fail();
+    }
+
+    /*
+     * Test for method "predicatedSortedBag(final SortedBag<E> bag, final Predicate<? super E> predicate)".
+     * It checks whether the method returns a PredicatedSortedBag type with the specified predicate (not null).
+     */
+    @Test
+    public void shouldReturnPredicatedSortedBag() {
+        SortedBag returnedBag = BagUtils.predicatedSortedBag(bagInstance, NotNullPredicate.INSTANCE);
+        assertTrue(returnedBag instanceof PredicatedSortedBag);
+        assertTrue(returnedBag.containsAll(items));
+        assertEquals(4, returnedBag.size());
+        assertEquals(1, returnedBag.first());
+        assertEquals(16, returnedBag.last());
+        try {
+            returnedBag.add(null);
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+            assertEquals(4, returnedBag.size());
+            return;
+        }
+        fail();
+    }
+
+    /*
+     * Test for method "predicatedSortedBag(final SortedBag<E> bag, final Predicate<? super E> predicate)".
+     * It checks whether the method throws a null pointer exception on null input (bag, or predicate).
+     */
+    @Test
+    public void shouldReturnPredicatedSortedBagNullInput() {
+        int i = 0;
+        try {
+            BagUtils.predicatedSortedBag(null, FalsePredicate.INSTANCE);
+        } catch (Exception e) {
+            assertTrue(e instanceof NullPointerException);
+            i++;
+        }
+        if(i == 0) {
+            fail();
+        }
+        try {
+            BagUtils.predicatedSortedBag(bagInstance, null);
+        } catch (Exception e) {
+            assertTrue(e instanceof NullPointerException);
+            return;
+        }
+        fail();
+    }
+
+    /*
+     * Test for method "transformingSortedBag(final SortedBag<E> bag, final Transformer<? super E, ? extends E> transformer)".
+     * It checks whether the method returns a TransformedSortedBag type with the specified transformer.
+     */
+    @Test
+    public void shouldReturnTransformedSortedBag() {
+        ConstantTransformer trans = new ConstantTransformer(77);
+        SortedBag returnedBag = BagUtils.transformingSortedBag(bagInstance, trans);
+        assertTrue(returnedBag instanceof TransformedSortedBag);
+        assertTrue(returnedBag.containsAll(items));
+        assertEquals(4, returnedBag.size());
+        returnedBag.add(30);
+        assertTrue(returnedBag.contains(77));
+        assertEquals(1, returnedBag.first());
+        assertEquals(77, returnedBag.last());
+        assertEquals(5, returnedBag.size());
+    }
+
+    /*
+     * Test for method "transformingSortedBag(final SortedBag<E> bag, final Transformer<? super E, ? extends E> transformer)".
+     * It checks whether the method throws a null pointer exception on null input (bag, or transformer).
+     */
+    @Test
+    public void shouldReturnTransformedSortedBagNullInput() {
+        int i = 0;
+        try {
+            BagUtils.transformingSortedBag(null, new PredicateTransformer(NotNullPredicate.INSTANCE));
+        } catch (Exception e) {
+            assertTrue(e instanceof NullPointerException);
+            i++;
+        }
+        if(i == 0) {
+            fail();
+        }
+        try {
+            BagUtils.transformingSortedBag(bagInstance, null);
+        } catch (Exception e) {
+            assertTrue(e instanceof NullPointerException);
+            return;
+        }
+        fail();
+    }
+
+    /*
+     * Test for method "emptyBag()".
+     * It checks whether the method returns an empty UnmodifiableBag type.
+     */
+    @Test
+    public void shouldReturnEmptyBag() {
+        Bag returnedBag = BagUtils.emptyBag();
+        assertTrue(returnedBag instanceof UnmodifiableBag);
+        assertTrue(returnedBag.isEmpty());
+    }
+
+    /*
+     * Test for method "emptySortedBag()".
+     * It checks whether the method returns an empty UnmodifiableSortedBag type.
+     */
+    @Test
+    public void shouldReturnEmptySortedBag() {
+        SortedBag returnedBag = BagUtils.emptySortedBag();
+        assertTrue(returnedBag instanceof UnmodifiableSortedBag);
+        assertTrue(returnedBag.isEmpty());
+    }
+}
